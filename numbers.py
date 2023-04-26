eab83f07e86ab7929e32e75b9749406ecb7362e0
maratdmitrichenki <maratdmitrichenki@gmail.com>
2023-04-26 16:43:30 +0300

    add task all

---
+++ numbers.py
@@ -0,0 +1,8 @@
+number_1 = int(input('Ведите первое число: '))
+number_2 = int(input('Ведите второе число: '))
+number_3 = int(input('Ведите третье число: '))
+number_4 = int(input('Ведите пятое число: '))
+
+result = (number_1 + number_2) / (number_3+number_4)
+
+print(float('{:.2f}'.format(result)))
